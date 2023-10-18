3b7a3287ee3aed9f3bf4ea3cd9718f5918fbadbb
Cristiano De Oliveira <cristiano.deoliveira@gmail.com>
2023-10-18 17:56:11 +0100

    new

---
+++ Exercise_Five7.py
@@ -0,0 +1,15 @@
+
+# My solution
+for letter in ['A', 'B', 'X', 'C', 'D', 'E']:
+
+    if (letter == 'A' or letter == 'B' or letter == 'C'):
+        print(letter)
+
+# Proposed solution
+# for letter in ['A', 'B', 'X', 'C', 'D', 'E']:
+#     if letter == 'D':
+#         break
+#     if letter == 'X':
+#         continue
+    
+#     print(letter)
