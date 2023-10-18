8c4c1688d0d9af289150fe6a30154bf53f3ac40e
Cristiano De Oliveira <cristiano.deoliveira@gmail.com>
2023-10-18 17:59:55 +0100

    commit

--- Exercise_Five7.py
+++ Exercise_Five7.py
@@ -4,7 +4,7 @@ for letter in ['A', 'B', 'X', 'C', 'D', 'E']:
 
     if (letter == 'A' or letter == 'B' or letter == 'C'):
         print(letter)
-
+#
 # Proposed solution
 # for letter in ['A', 'B', 'X', 'C', 'D', 'E']:
 #     if letter == 'D':
